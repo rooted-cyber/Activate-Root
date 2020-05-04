3c0c653bca52541806f77084ec39688a821769a9
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2020-05-04 08:14:13 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Activate-Root
+# Inatallation
+<pre>
+ $ apt update
+ $ apt upgrade
+ $ apt install git
+ $ apt install neofetch
+ $ apt install python
+ $ apt install figlet
+ $ apt install proot
+ $ git clone https://github.com/rooted-cyber/Activate-Root
+ $ cd Activate-Root
+ $ bash Root.sh
+ </pre>
+ 
+ <pre>
+ Usages :-
+ sudo
+ </pre>
---
+++ Root.sh
@@ -0,0 +1,72 @@
+R () {
+	cd $PREFIX/bin
+	if [ -e python ];then
+	python ~/Activate-Root/.random.py
+	else
+	pkg install python
+	python ~/Activate-Root/.random.py
+	fi
+	}
+	b() {
+	R
+	figlet root
+	echo
+	}
+	
+	
+	
+		packages () {
+			cd $PREFIX/bin
+			if [ -e proot ];then
+			R
+			printf "\n Proot is installed \n"
+			else
+			apt update
+			apt upgrade
+			apt install proot
+			fi
+			if [ -e figlet ];then
+			R
+			printf "\n Figlet is installed\n"
+			else
+			apt update
+			apt upgrade
+			apt install figlet
+			clear
+			fi
+			if [ -e neofetch ];then
+			R
+			printf "\n Neofetch is installed\n"
+			else
+			apt update
+			apt upgrade
+			apt install root
+			clear
+			fi
+			sleep 2
+			}
+			roott () {
+				R
+				printf "\n\n Adding sudo command\n"
+				cd ~/Activate-Root
+				chmod 1111 *
+				cp -f sudo $PREFIX/bin
+				chmod 777 sudo
+				R
+				clear
+				R
+				printf "\n\n Now you can use this command :- sudo\n"
+				echo
+				echo
+				}
+				menu () {
+					b
+		echo -e -n "\033[93m Do you want to activate root\033[94m(\033[96mY/N\033[94m) "
+		read a
+		case $a in
+		y|Y)roott ;;
+		n|N)exit  ;;
+		esac
+		}
+		packages
+		menu
